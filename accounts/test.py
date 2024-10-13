697104deb59a3f9e1eb269e83f833d8944b8737b
JingyuZhang <j.zhang.73@student.scu.edu.au>
2024-10-14 04:57:59 +0800

    Update test.py

--- accounts/test.py
+++ accounts/test.py
@@ -1,1 +1,15 @@
+# settings.py
+from decouple import config
 
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='5432'),
+    }
+}
