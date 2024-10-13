724a655346627d8d0340450ae7cadbc0c506939d
JingyuZhang <j.zhang.73@student.scu.edu.au>
2024-10-13 20:51:40 +0800

    Create appointment.py

---
+++ appointment.py
@@ -0,0 +1,13 @@
+# appointment.py
+
+def create_appointment(patient_id, doctor_id, time):
+    # 伪代码：创建预约
+    pass
+
+def view_appointment(appointment_id):
+    # 伪代码：查看预约
+    pass
+
+def cancel_appointment(appointment_id):
+    # 伪代码：取消预约
+    pass
